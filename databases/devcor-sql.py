a3f9df9cf5695c6ddabd586e74b94b6e14641454
boblongmore <robertlongmore@gmail.com>
2020-04-07 10:13:52 -0500

    updated for preso

--- databases/devcor-sql.py
+++ databases/devcor-sql.py
@@ -11,30 +11,47 @@ connection = engine.connect()
 Base = declarative_base()
 
 #define our tables
-class albums(Base):
-    __tablename__ = 'albums'
+class customer(Base):
+    __tablename__ = 'customers'
     Id = db.Column(db.Integer(), primary_key=True, unique=True)
-    Title = db.Column(db.String(255))
+    FirstName = db.Column(db.String(255))
+    LastName = db.Column(db.String(255))
+    Email = db.Column(db.String(255))
+    ZipCode = db.Column(db.Integer())
 
-class artists(Base):
-    __tablename__ = 'artists'
+class order(Base):
+    __tablename__ = 'orders'
     Id = db.Column(db.Integer(), primary_key=True, unique=True)
-    artist = db.Column(db.String(255))
+    Date = db.Column(db.String())
+    Amount = db.Column(db.Float())
+    CustomerId = db.Column(db.Integer, db.ForeignKey('customers.Id'))
 
 
 Base.metadata.create_all(engine)
 
 session = sessionmaker(bind=engine)
 my_session = session()
-new_artist = artists(artist="Bob Dylan")
+new_c = customer(FirstName="Bob", LastName="Dylan", Email="bob@dylan.com", ZipCode=55417)
 
-my_session.add(new_artist)
+my_session.add(new_c)
 
 my_session.commit()
-my_session.query(artists).all()
+my_session.query(customer).all()
 
-for each_artist in my_session.query(artists).all():
-    print(each_artist.artist)
+for each_c in my_session.query(customer).all():
+    print(each_c.FirstName, each_c.LastName)
+
+dylan_id = my_session.query(customer).filter_by(LastName='Dylan').all()
+for records in dylan_id:
+    fk = records.Id
+new_order = order(Date="2020-04-07", Amount="56.08", CustomerId=fk)
+my_session.add(new_order)
+my_session.commit()
+for each_order in my_session.query(order).all():
+    print(each_order.Amount, each_order.CustomerId)
+
+
+
 
 
 
